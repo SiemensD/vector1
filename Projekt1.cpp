6df5ea530ed24b42a74e3981b9585fdabf08b0f1
SiemensD <d.siemens71@gmail.com>
2021-08-30 16:37:11 +0300

    Добавьте файлы проекта.

---
+++ Projekt1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void main()
+{
+	vector<int>myVector(10);
+
+	for (int i = 0; i < myVector.size(); i++)
+	{
+		myVector[i] = (i+1) * (i+1);
+	}
+
+	for (int i = 0; i < myVector.size(); i++)
+	{
+		cout<<myVector[i]<<endl;
+	}
+}
+/*1.Заполнить вектор размером 10 квадратом целых чисел и вывести его ы выходной поток*/
